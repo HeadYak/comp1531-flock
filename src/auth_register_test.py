21cd952fbc515993d1bdf713233a3fc933324d44
z5264786 <z5264786@cse.unsw.edu.au>
2020-09-21 00:38:24 +1000

    Some test cases for auth_register

---
+++ src/auth_register_test.py
@@ -0,0 +1,28 @@
+from auth import auth_register , auth_login
+import pytest
+import echo
+from error import InputError
+
+def test_auth_register_BaseCase():
+    result = auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert result == {
+        'u_id': 1,
+        'token': '12345',
+    }
+
+#Test case for attempting to register using an invalid email address
+def test_auth_register_InvalidEmail():
+    with pytest.raises(InputError):
+        result = auth_register('Thisisnotanemailaddress', '123abc!@#', 'Hayden', 'Everest')
+
+
+#Test case for when attempting to register with an email already used to register
+def test_auth_register_ExistingEmail():
+    with pytest.raises(InputError):
+        result = auth_register('existingemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')   
+
+
+#Test case when attempting to register with a password shorter than 6 characters
+def test_auth_register_InvalidPassword():
+    with pytest.raises(InputError):
+        result = auth_register('validemail@gmail.com', '123', 'Hayden', 'Everest')  
