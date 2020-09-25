7310bec297a49ca941b79b8c717e3fd2ab657877
Neve Parsons <z5257072@cse.unsw.edu.au>
2020-09-25 17:33:10 +1000

    changed names of some varibles in channels_list_test

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -2,7 +2,7 @@ from channels import channels_listall
 from auth import auth_register
 import pytest
 
-def channels_list_test():
+def channels_listall_test():
     #Creating users to create channels
     token1 = auth_register("user1@gmail.com", user1pass, user1, last1).token()
     token2 = auth_register("user2@gmail.com", user2pass, user2, last2).token()
@@ -19,10 +19,10 @@ def channels_list_test():
     ch_id4 =channels_create(token2, SameName, False)
 
     #should return list all the channels created
-    assert channels_listall(token1) == [{'id': 1, 'name': "aGreatChannel", 'public': True},
-                                        {'id': 2, 'name': "yetAnotherChannel", 'public': False},
-                                        {'id': 3, 'name': "SameName", 'public': True},
-                                        {'id': 4, 'name': "SameName", 'public': False}]
+    assert channels_listall(token1) == [{'id': 1, 'name': "aGreatChannel", 'is_public': True},
+                                        {'id': 2, 'name': "yetAnotherChannel", 'is_public': False},
+                                        {'id': 3, 'name': "SameName", 'is_public': True},
+                                        {'id': 4, 'name': "SameName", 'is_public': False}]
                                     
                                     
 
