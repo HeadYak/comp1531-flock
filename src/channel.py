b0a0abbdea846c892ce4f3a107a3bd190c97a593
Neve Parsons <z5257072@cse.unsw.edu.au>
2020-09-27 16:07:05 +1000

    wrote channel leave, join and invite functions

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,29 @@
+from global_data import users, channels
+import pytest
+from error import InputError
+from helper_functions import user_in_channel, user_exists, channel_exists, create_member, get_u_id
+
+
 def channel_invite(token, channel_id, u_id):
-    return {
-    }
+    
+    
+    if channel_exists(channel_id) == False:
+        raise InputError('Invalid channel')
+        
+    elif user_exists(u_id) == False:
+        raise InputError('Invalid user id')
+    
+    authorised_u_id == get_u_id(token)
+    
+    elif user_in_channel(authorised_u_id, channel_id) == False:
+        raise InputError('User not a member of channel')
+       
+    else:
+        for channel in channels:
+            if channel['channel_id'] == channel_id:
+                channel['members'].append(create_member(u_id))   
+    
+    return {}
 
 def channel_details(token, channel_id):
     return {
@@ -36,12 +59,46 @@ def channel_messages(token, channel_id, start):
     }
 
 def channel_leave(token, channel_id):
-    return {
-    }
+
+     if channel_exists(channel_id) == False:
+        raise InputError('Invalid channel')
+    
+    authorised_u_id == get_u_id(token)
+    
+    elif user_in_channel(authorised_u_id, channel_id) == False:
+        raise InputError('User not a member of channel')
+    
+    else:
+        for channel in channels:
+            if channel['channel_id'] == channel_id:
+                for member in channel['members']:
+                    if member['u_id'] == u_id:
+                       channel['members']. remove(member)
+    
+    return {}
 
 def channel_join(token, channel_id):
-    return {
-    }
+    
+    u_id == get_u_id(token)
+    
+    public = False 
+    for channel in channels:
+        if channel['channel_id'] == channel_id:
+            if channel['is_public'] == True:
+                public = True
+    
+    if public == False:
+        raise InputError('Private Channel')
+    
+    elif channel_exists(channel_id) == False:
+        raise InputError('Invalid channel')
+    
+    else:
+        for channel in channels:
+            if channel['channel_id'] == channel_id:
+                channel['members'].append(create_member(u_id))   
+        
+    return {}
 
 def channel_addowner(token, channel_id, u_id):
     return {
