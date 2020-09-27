c823c5c2ed011c32a654c3772581ece86b51dc6d
z5264786 <z5264786@cse.unsw.edu.au>
2020-09-27 23:25:55 +1000

    Channels create done

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from global_data import channels, users
+import pytest
+from error import InputError
 def channels_list(token):
     return {
         'channels': [
@@ -19,6 +22,33 @@ def channels_listall(token):
     }
 
 def channels_create(token, name, is_public):
-    return {
-        'channel_id': 1,
-    }
+    print(token)
+    print(users)
+    valid_token = False
+    for user in users:
+        if(user['token'] == token):
+            valid_token = True
+
+
+            
+
+
+
+    if(valid_token == True and len(name) <= 20):
+        new_channel = {
+            'channel_id': len(channels)+1,
+            'name': name,
+            'is_public': is_public,
+            'creator': token,
+            'owners': [],
+            'messages': []
+        }
+        new_channel_copy = new_channel.copy()
+        channels.append(new_channel_copy)
+
+        return {
+            'channel_id': new_channel_copy['channel_id']
+        }
+    if(len(name) > 20):
+        raise InputError('Invalid Name')
+
