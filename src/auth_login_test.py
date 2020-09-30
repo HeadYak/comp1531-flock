4077914856cbc29e17b045fe22fc13745b932a55
z5264786 <z5264786@cse.unsw.edu.au>
2020-09-30 22:21:10 +1000

    Merge with auth_login_test

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -3,30 +3,32 @@ from auth import auth_register
 import echo 
 import pytest
 from error import InputError
+from global_data import users
 import re
-
+from other import clear
 # Regular expression for validating an email
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
 def test_auth_login_BaseCase():
-    if (re.search(regex, email):
-        result = auth_login('validemail@gmail.com', '123abc!@#')
-        assert result == {
-            'u_id': ' '
-            'token': ' ',
-        }
+    clear()
+    auth_register('anothervalidemail@gmail.com', '123abc!@#*', 'Howard', 'Everton')
+    print(users)
+    result = auth_login('anothervalidemail@gmail.com', '123abc!@#*')
+    assert len(result) == 2
 
-# Testing for attempt to login using an invalid email address 
+#Testing for attempt to login using an invalid email address 
+    clear()
 def test_auth_login_InvalidEmail():
     with pytest.raises(InputError):
         auth_login('Notavalidemailaddress', '123abc!@#')
     
 # Testing for attempt to login using an email address that does not belong to a user
+    clear()
 def test_auth_login_NonExistingEmail():
     with pytest.raises(InputError):
         auth_login('Emaildoesnotbelongtoauser', '123abc!@#')
 
 # Testing given an incorrect password 
+    clear()
 def test_auth_login_IncorrectPassword():
     with pytest.raises(InputError):
         auth_login('validemail@gmail.com', 'abc')
