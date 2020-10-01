115a58c4f971270b11952088ec0ad96f4ff46640
z5264786 <z5264786@cse.unsw.edu.au>
2020-10-01 14:17:50 +1000

    Readded messages data struct

--- src/global_data.py
+++ src/global_data.py
@@ -1,7 +1,8 @@
 
-#channels = [{"channel_id": , "name": , "messages" :[] , "owners": [] , "members": [{"u_id:" , "name_first:" , "name_last:"}], "owners": [{"u_id:" , "name_first:" , "name_last:"}]}]
+#channels = [{"channel_id": , "name": , "message", "owners": [] , "members": [{"u_id:" , "name_first:" , "name_last:"}], "owners": [{"u_id:" , "name_first:" , "name_last:"}]}]
 channels = []
 
 #users = [{"u_id: " , "token: ", "email: ", "password: ", "name_first: ", "name_last: "}]
 users = []
 
+messages = []
