65be6e902f060bb63b55c6b61bd87ccd2ec7f872
z5264786 <z5264786@cse.unsw.edu.au>
2020-10-01 14:07:19 +1000

    Changed messages data type

--- src/global_data.py
+++ src/global_data.py
@@ -1,11 +1,6 @@
 
-#channels = [{"channel_id": , "name": , "owners": [] , "members": [{"u_id:" , "name_first:" , "name_last:"}], "owners": [{"u_id:" , "name_first:" , "name_last:"}]}]
+#channels = [{"channel_id": , "name": , "messages" :[] , "owners": [] , "members": [{"u_id:" , "name_first:" , "name_last:"}], "owners": [{"u_id:" , "name_first:" , "name_last:"}]}]
 channels = []
-
-
-# messages = [{ "message_id":, "u_id":, "message":, "time_created":}]
-messages = []
-
 
 #users = [{"u_id: " , "token: ", "email: ", "password: ", "name_first: ", "name_last: "}]
 users = []
