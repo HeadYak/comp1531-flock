5b4906d0ae36f21c81fad2795b5b17df1494154d
z5264786 <z5264786@cse.unsw.edu.au>
2020-10-18 02:52:22 +1100

    Start of server routes

--- src/server.py
+++ src/server.py
@@ -4,6 +4,18 @@ from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
 
+# from auth import auth_login, auth_logout, auth_register
+# from channel import channel_invite, channel_details, channel_messages,channel_leave,channel_join,channel_addowner,channel_removeowner
+# from channels import channels_list, channels_listall, channels_create
+# from message import message_send, message_remove, message_edit
+from auth import * 
+from channel import *
+from channels import *
+from message import *
+from global_data import *
+from helper_functions import *
+from other import *
+from user import *
 def defaultHandler(err):
     response = err.get_response()
     print('response', err, err.get_response())
@@ -31,5 +43,27 @@ def echo():
         'data': data
     })
 
+@APP.route('/auth/login', methods=['POST'])
+def authlogin():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    return dumps({
+        'email': email,
+        'password': password
+    })
+
+@APP.route('/auth/register', methods=['POST'])
+def authregister():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    name_first = request.args.get('name_first')
+    name_last = request.args.get('name_last')
+    return dumps({
+        'email': email,
+        'password': password,
+        'name_first': name_first,
+        'name_last': name_last
+    })
+
 if __name__ == "__main__":
-    APP.run(port=0) # Do not edit this port
+    APP.run(port=0 , debug=True) # Do not edit this port
