b184652fb96acd6c07c336ef037e0449e6cd9f72
Irene <z5309942@ad.unsw.edu.au>
2020-10-19 09:24:20 +1100

    Changed to add only check function

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,18 +1,10 @@
-'''
-Nessacary imports
-'''
-import jwt
-import re 
 from global_data import users, channels
-
-SECRET = 'orangeTeam5'
+import re
 
 regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
+#checks if user is in channel
 def user_in_channel(u_id, channel_id):
-    '''
-    checks if user is in channel
-    '''
     found = False
     for channel in channels:
         if channel['channel_id'] == channel_id:
@@ -22,84 +14,70 @@ def user_in_channel(u_id, channel_id):
     return found
 
 def user_is_owner(u_id, channel_id):
-    '''
-    checks is user is owner
-    '''
     found = False
     for channel in channels:
-        if channel['channel_id'] == channel_id:
+        if (channel['channel_id'] == channel_id):
+            # print('hello: ' ,channel)
             for owner in channel['owners']:
-                if owner['u_id'] == u_id:
+                if (owner['u_id'] == u_id):
                     found = True
     return found
 
+
 def user_is_creator(u_id, channel_id):
-    '''
-    checks is user is creator of channel
-    '''
     result = False
     for channel in channels:
-        if channel['channel_id'] == channel_id:
+        if (channel['channel_id'] == channel_id):
             if channel['creator']['u_id'] == u_id:
                 result = True
     return result
 
+
+#checks if channel exists
 def channel_exists(channel_id):
-    '''
-    checks if channel exists
-    '''
-    chan_exists = False
+    channel_exists = False
     for channel in channels:
-        if channel['channel_id'] == channel_id:
-            chan_exists = True
-            return chan_exists
-    return chan_exists
+        print(channel['channel_id'])
+        if (channel['channel_id'] == channel_id):
+            channel_exists = True
+            
+    return channel_exists
 
-def user_exists(u_id):
-    '''
-    checks if user exists
-    '''
-    user = False
+#checks if user exists
+def user_exists(u_id):            
+    user_exists = False
     for user in users:
-        if user['u_id'] == u_id:
-            user = True
+        if (user['u_id'] == u_id):
+            user_exists = True
+            
     return user_exists
-
+    
+#checks if get_u_id exists
 def get_u_id(token):
-    '''
-    gets user id given their token
-    '''
-    decoded = jwt.decode(token, SECRET, algorithms=['HS256'])
-    return decoded['u_id']
+    for user in users:
+        if (user['token'] == token):
+            return user['u_id']
 
+#given a token finds users u_id
 def get_token(u_id):
-    '''
-    Gets token given u_id
-    '''
     for user in users:
-        if user['u_id'] == u_id:
-            return user['token']
+        if (user['u_id'] == u_id):
+            return user['token']    
 
-    return None
-
+#creates a member dictionary
 def create_member(u_id):
-    '''
-    creates a member dictionary for user
-    '''
     for user in users:
-        if user['u_id'] == u_id:
+        if (user['u_id'] == u_id):
             name_first = user['name_first']
             name_last = user['name_last']
+    
     return {
         'u_id': u_id,
         'name_first': name_first,
         'name_last': name_last,
     }
-
+   
 def user_a_member(u_id, channel_id):
-    '''
-    check if user is a member of a channel
-    '''
     found = False
     for channel in channels:
         if channel['channel_id'] == channel_id:
@@ -107,40 +85,6 @@ def user_a_member(u_id, channel_id):
                 if owners['u_id'] == u_id:
                     found = True
     return found
-
-def message_exists(message_id):
-    '''
-    checks if message exists
-    '''
-    found = False
-    for channel in channels:
-        for chan_messages in channel['messages']:
-            if chan_messages['message_id'] == message_id:
-                found = True
-                return found
-    return found
-
-def message_creator(u_id, message_id):
-    '''
-    checks is user is creator of the message
-    '''
-    found = False
-    for channel in channels:
-        for chan_messages in channel['messages']:
-            if chan_messages['creator'] == u_id and chan_messages['message_id'] == message_id:
-                found = True
-                return found
-    return found
-
-def find_channel(message_id):
-    '''
-    find the channel a message belongs to
-    '''
-    for channel in channels:
-        for msg in channel['messages']:
-            if msg['message_id'] == message_id:
-                return channel['channel_id']
-    return None
 
 def check(email):
     '''
