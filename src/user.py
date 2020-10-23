7fead7a951b1817fefb04bed4cb7865290ec8e0d
Irene <z5309942@ad.unsw.edu.au>
2020-10-23 13:36:57 +1100

    Added some comments, fixed spacing

--- src/user.py
+++ src/user.py
@@ -4,14 +4,17 @@ from global_data import users
 from helper_functions import get_u_id, check, user_exists
 
 def user_profile(token, u_id):
-    # retrieve u_id from token
+    '''
+    For a valid user, return information on the user  
+    '''
     new_u_id = get_u_id(token)
-    # raise error if not a valid user
-    #user['token'] = jwt.encode({'u_id': user['u_id']}, SECRET, algorithm='HS256')
-    
+    '''
+    Raise error if user with u_id is not a valid user 
+    '''
     for user in users:
         if user_exists(new_u_id) and new_u_id != u_id:
             raise InputError('Invalid User')
+    
     for user in users:
         if user['u_id'] == u_id:
             return {
@@ -24,11 +27,15 @@ def user_profile(token, u_id):
 
 def user_profile_setname(token, name_first, name_last):
     u_id = get_u_id(token)
-    # Checks if the length of the user's name is within the set restrictions
+    '''
+    Checks if the length of the user's name is within the set restrictions
+    '''
     if (len(name_first) < 1 or len(name_first) > 50):
         raise InputError('Invalid First Name')
+    
     if (len(name_last) < 1 or len(name_last) > 50):
         raise InputError('Invalid Last Name')
+
     for user in users:
         if user['u_id'] == u_id:
             user['name_first'] == name_first
@@ -37,13 +44,18 @@ def user_profile_setname(token, name_first, name_last):
 
 def user_profile_setemail(token, email):
     u_id = get_u_id(token)
-    # Email entered is not a valid email
+    '''
+    Checking if the email entered is not a valid email 
+    '''
     if check(email) == False:
         raise InputError('Invalid Email')
-    # Email address is already being used by another user
+    '''
+    Making sure that the email address is not already being used by another user
+    '''
     for user in users:
         if user['email'] == email:
             raise InputError('Email is already being used')
+
     for user in users:
         if user['u_id'] == u_id:
             user['email'] == email
