709afdc77079eadd9d66a9b19ddd2bbcb3c3c122
Neve Parsons <z5257072@cse.unsw.edu.au>
2020-11-08 15:56:48 +1100

    Wrote backend tests for user_profile_uploadphoto

--- src/user_tests/user_profile_uploadphoto_test.py
+++ src/user_tests/user_profile_uploadphoto_test.py
@@ -10,30 +10,31 @@ def test_user_profile_uploadphoto():
 
     clear()
 
-    user = auth_register('email1@gmail.com', 'password1', 'user1', 'userlast1')
+    user = auth_register('email1@gmail.com', 'password1', 'user1', 'userlast1', None)
     token = user['token']
 
     with pytest.raises(InputError):
         #Inavlid coordinates
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,5,9,12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,-5,9,12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,5,-9,12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,5,9,-12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,-5,-9,-12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,-5,9,12)
-        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,-5,-5)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,5,9,12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,-5,9,12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,5,-9,12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 1,5,9,-12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,-5,-9,-12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', -1,-5,9,12 , None)
+        user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,-5,-5, None)
   
     with pytest.raises(InputError):
         #Invalid url
-        user_profile_uploadphoto(token, 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw.jpg', 0,0,50,50)
+        user_profile_uploadphoto(token, 'https://helpx.adobe.com/content/dam/help/en/stock/visual-reverse-image-search-v2_297x176.jpg', 0,0,50,50, None)
         user_profile_uploadphoto(token, '', 0,0,50,50)
 
     with pytest.raises(InputError):
         #image not a jpeg
-        user_profile_uploadphoto(token, 'https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png', 0,0,50,50)
+        user_profile_uploadphoto(token, 'https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png', 0,0,50,50, None)
 
     #No error message should appear
-    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 6,6,60,60)
-    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,100,100)
-    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,1,1)
+    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 6,6,60,60, None)
+    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,100,100, None)
+    user_profile_uploadphoto(token, 'https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1024%2Cc_limit/phonepicutres-TA.jpg', 0,0,1,1, None)
 
+    user_profile_uploadphoto(token, 'https://pyxis.nymag.com/v1/imgs/cbe/f4c/00a849914d501ee4b30ef830a8662bff2c-08-pepe-the-frog.rsquare.w700.jpg', 0,0,600,600, None)
