3a7c40249b41ba5059c0855cce3e5e96058bc121
Irene <z5309942@ad.unsw.edu.au>
2020-11-10 01:53:54 +1100

    Testing for message unpin server

---
+++ src/message_http_tests/message_unpin_http_test.py
@@ -0,0 +1,54 @@
+'''
+HTTP tests for message pin
+'''
+import requests
+SECRET = 'orangeTeam5'
+
+def test_message_pin_http():
+    '''
+    HTTP test for auth register
+    '''
+    #creating data for user
+    data = {
+        'email': 'email@gmail.com',
+        'password': 'HELLO123@',
+        'name_first': 'Frank',
+        'name_last': 'Su'
+    }
+
+    #registering user
+    user = requests.post(f"{url}/auth/register", json=data)
+    user_dict = user.json()
+
+    #creating channel data
+    channel_data = {
+        'token': user_dict['token'],
+        'name': "Test_channel",
+        'is_public': True
+    }
+
+    #Creating a channel
+    channel = requests.post(f"{url}channels/create", json=channel_data)
+    channel_dict = channel.json()
+
+    #creating message data
+    message_data = {
+        'token': user_dict['token'],
+        'channel_id': channel_dict['channel_id'],
+        'message': 'hello'
+    }
+
+    #sending a message
+    message = requests.post(f"{url}/message/send", json=message_data)
+    message_dict = message.json()
+
+    #pinning a message
+    pinned_message = requests.post(f"{url}/message/pin", json=message_data)
+    pinned_message_dict = pinned_message.json()
+    
+    #testing message unpin
+    resp = requests.post(f"{url}/message/unpin", json=message_data)
+    assert resp.status_code == 200
+    resp_dict = resp.json()
+    assert resp_dict == {}
+
