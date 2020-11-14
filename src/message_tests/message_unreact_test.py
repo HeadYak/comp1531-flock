004e77bbe5006195ff57ad835ae20645e6c658aa
William Tran <z5310085@ad.unsw.edu.au>
2020-11-14 12:29:22 +1100

    Fixed a test case in message_react tests, and added message_unreact tests

--- src/message_tests/message_unreact_test.py
+++ src/message_tests/message_unreact_test.py
@@ -2,16 +2,17 @@
 Required modules
 '''
 import pytest
-from message import message_send, message_react
+from message import message_send, message_react, message_unreact
 from channels import channels_create
 from auth import auth_register
 from error import InputError
 from helper_functions import getChannelData, resetData
 
-def test_messagereact_base():
+def test_messageunreact_base():
     '''
-    Testing if a react can be added to a message
+    Testing if react can be removed from a message
     '''
+    
     resetData()
 
     # Registering user and creating a channel to send messages to
@@ -21,24 +22,20 @@ def test_messagereact_base():
 
     m_id = message_send(token1, ch_id1, 'hey')
     message_react(token1, m_id, 1)
-
+    message_unreact(token1, m_id, 1)
+    
     data = getChannelData()
     channel_list = data['channels']
-
+    
     for channel in channel_list:
         for message in channel['messages']:
-            assert len(message['reacts']) == 1
-            for react in message['reacts']:
-                if react['react_id'] == 1:
-                    assert len(react['u_ids']) == 1
-                    assert user1['u_id'] in react['u_ids']
-                    break
+            assert len(message['reacts']) == 0
             break
         break
         
     resetData()
 
-def test_messagereact_invalid_input():
+def test_messageunreact_invalid_input():
     '''
     Testing if errors returned when given invalid input
     '''
@@ -54,22 +51,24 @@ def test_messagereact_invalid_input():
 
     m_id1 = message_send(token1, ch_id1, 'hey')
     m_id2 = message_send(token2, ch_id2, 'hello')
-
-    # Trying to react to a non_existing message
-    with pytest.raises(InputError):
-        message_react(token1, m_id1 + 100, 1)
-
-    # Trying to react with a non_existing react type
-    with pytest.raises(InputError):
-        message_react(token1, m_id1, 1000)
-
-    # Trying to react to a message in a channel user has not joined
-    with pytest.raises(InputError):
-        message_react(token1, m_id2, 1)
-
-    # Trying to react to the same message with the same react
+    
     message_react(token1, m_id1, 1)
+    
+    # Trying to remove react from a non_existing message
     with pytest.raises(InputError):
-        message_react(token1, m_id1, 1)
+        message_unreact(token1, m_id1 + 100, 1)
+
+    # Trying to unreact from a non_existing react type
+    with pytest.raises(InputError):
+        message_unreact(token1, m_id1, 1000)
+
+    # Trying to unreact to a message in a channel user has not joined
+    with pytest.raises(InputError):
+        message_unreact(token1, m_id2, 1)
+
+    # Trying to unreact to a message with no reacts of that type
+    message_unreact(token1, m_id1, 1)
+    with pytest.raises(InputError):
+        message_unreact(token1, m_id1, 1)
 
     resetData()
